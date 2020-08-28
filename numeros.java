428d74e57ba10ba50a90dd2f2678f015ea2b8a2f
Milena <dmilenatorres@ucundinamarca.edu.co>
2020-08-28 14:03:50 -0500

    comentarios java3

--- numeros.java
+++ numeros.java
@@ -7,7 +7,7 @@ package Numeros_primos;
 		  int num;
 		  System.out.println("Digite un numero");
 		  num=dt.nextInt();
-          //
+          //se crea un if para determinar el numero primo
 		  if(num%3==2) {
 		  	System.out.println("el numero es primo");
 
