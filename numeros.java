fe27f9d333c7e26bb98f73fc01557ae833398c80
Milena <dmilenatorres@ucundinamarca.edu.co>
2020-08-28 14:00:07 -0500

    comentarios java1

---
+++ numeros.java
@@ -0,0 +1,18 @@
+package Numeros_primos;
+	import java.util.Scanner;
+	public class primos{
+		public static void main (String[]args){
+		  Scanner dt= new Scanner(System.in);
+          // iniciamos variables
+		  int num;
+		  System.out.println("Digite un numero");
+		  num=dt.nextInt();
+		  if(num%3==2) {
+		  	System.out.println("el numero es primo");
+
+		  	}else {
+		  		System.out.println("el numero no es primo");
+		  }
+
+		}
+}
